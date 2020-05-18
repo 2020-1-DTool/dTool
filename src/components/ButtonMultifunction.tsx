3ae28081be15bf7f170c3e1692010299be55f4b5
João Brentano <joaobrentano.br@gmail.com>
2020-05-18 00:04:28 -0300

    Início do desenvolvimento

--- src/components/ButtonMultifunction.tsx
+++ src/components/ButtonMultifunction.tsx
@@ -7,15 +7,26 @@ import {
 } from "react-native";
 import colors from "../utils/colors";
 import sizes from "../utils/sizes";
+import { Style } from "react-native-material-kit";
+import { Image } from "react-native";
 
 export interface Props extends TouchableOpacityProps {
   disabled?: boolean;
   title: string;
+  //onPress: Function;
+  //onPress?: (index: number) => void;
+  style: Style;
+  text: string;
+  action: string;
 }
 
 const ButtonMultifunction: React.FC<Props> = ({
   disabled,
   title,
+  //onPress,
+  style,
+  text,
+  action,
   ...props
 }) => {
   const buttonStyle = disabled ? styles.buttonDisabled : styles.button;
@@ -27,6 +38,10 @@ const ButtonMultifunction: React.FC<Props> = ({
       style={[styles.base, buttonStyle]}
     >
       <Text style={styles.text}>{title}</Text>
+      <Image
+        style={styles.icon}
+        source={require("../assets/" + action + ".png")}
+      />
     </TouchableOpacity>
   );
 };
@@ -35,8 +50,11 @@ const styles = StyleSheet.create({
   base: {
     borderRadius: 8,
     justifyContent: "center",
-    minHeight: 45,
+    alignItems: "center",
+    minHeight: 50,
     minWidth: 328,
+    flexDirection: "row",
+    position: "relative",
   },
   button: {
     backgroundColor: colors.theme.primary,
@@ -45,10 +63,15 @@ const styles = StyleSheet.create({
     backgroundColor: colors.basic.separator,
   },
   text: {
-    alignSelf: "center",
     color: colors.basic.background,
     fontSize: sizes.buttonText.main,
     fontWeight: "600",
+    position: "absolute",
+    left: 16,
+  },
+  icon: {
+    position: "absolute",
+    right: 16,
   },
 });
 
