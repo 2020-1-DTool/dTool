912a14b00fe0c8cffd88191968cd5e1cc009ced1
Camila borba <camillaborbarocha@gmail.com>
2020-05-20 01:12:08 -0300

    ajustes que foram apontados no code-review

--- src/screens/CarouselScreen.tsx
+++ src/screens/CarouselScreen.tsx
@@ -2,24 +2,17 @@ import React from "react";
 import { Dimensions, SafeAreaView, StyleSheet, View } from "react-native";
 
 import { StackNavigationProp } from "@react-navigation/stack";
-import { ButtonPrimary } from "../components";
 import { Carousel } from "../containers";
 
 export interface ScreenProps {
   navigation: StackNavigationProp<any, any>;
 }
 
-const CarouselScreen: React.FC<ScreenProps> = ({ navigation }) => {
+const CarouselScreen: React.FC<ScreenProps> = () => {
   return (
     <SafeAreaView>
       <View style={styles.body}>
         <Carousel />
-        <ButtonPrimary
-          title="Teste"
-          onPress={() => {
-            return navigation.navigate("EmptyScreen");
-          }} // remover modificação futuramente, apenas para testes
-        />
       </View>
     </SafeAreaView>
   );
