595bfcd1e629b8a74eda499cd8c2e422dcb35760
bianca-camargo-machado1 <bianca.camargo.machado1@hp.com>
2020-05-23 20:43:21 -0300

    card items position

--- src/containers/CardDescription.tsx
+++ src/containers/CardDescription.tsx
@@ -19,58 +19,54 @@ export interface ScreenProps {
 
 const CardDescription: React.FC<ScreenProps> = ({ data }) => {
   return (
-    <View>
-      <Card containerStyle={styles.cardStyle}>
-        <View />
-        <View>
-          {data?.map((item) => (
-            <View key={item.id}>
-              <View style={styles.cardTitle}>
-                <Text style={styles.boldText}>{item.title}</Text>
-              </View>
-              <View style={styles.cardInfo}>
-                <Image
-                  style={styles.imagePadding}
-                  source={require("../assets/profile-carousel.png")}
-                />
-                <Text style={styles.normalText}>{item.patient}</Text>
-              </View>
-              <View style={styles.cardInfo}>
-                <Image
-                  style={styles.imagePadding}
-                  source={require("../assets/clock-carousel.png")}
-                />
-                <Text style={styles.normalText}>{item.time}</Text>
-              </View>
-              <View style={styles.cardInfo}>
-                <Image
-                  style={styles.imagePadding}
-                  source={require("../assets/profile-carousel.png")}
-                />
-                <Text style={styles.normalText}>{item.role}</Text>
-              </View>
+    <Card containerStyle={styles.cardStyle}>
+      <View style={styles.container}>
+        {data?.map((item) => (
+          <View key={item.id}>
+            <View style={styles.cardTitle}>
+              <Text style={styles.boldText}>{item.title}</Text>
             </View>
-          ))}
-        </View>
-        <View style={styles.buttonsWrap}>
-          <View style={styles.buttonsCardDescription}>
-            <ButtonExecutions
-              onPress={() => "nothingyet"}
-              action="start"
-              text="INICIAR"
-            />
+            <View style={styles.cardInfo}>
+              <Image
+                style={styles.imagePadding}
+                source={require("../assets/profile-carousel.png")}
+              />
+              <Text style={styles.normalText}>{item.patient}</Text>
+            </View>
+            <View style={styles.cardInfo}>
+              <Image
+                style={styles.imagePadding}
+                source={require("../assets/clock-carousel.png")}
+              />
+              <Text style={styles.normalText}>{item.time}</Text>
+            </View>
+            <View style={styles.cardInfo}>
+              <Image
+                style={styles.imagePadding}
+                source={require("../assets/profile-carousel.png")}
+              />
+              <Text style={styles.normalText}>{item.role}</Text>
+            </View>
           </View>
-          <View style={styles.buttonsCardDescription}>
-            <ButtonExecutions
-              onPress={() => "nothingyet"}
-              action="cancel"
-              text="CANCELAR"
-            />
-          </View>
+        ))}
+      </View>
+      <View style={styles.buttonsWrap}>
+        <View style={styles.buttonsCardDescription}>
+          <ButtonExecutions
+            onPress={() => "nothingyet"}
+            action="start"
+            text="INICIAR"
+          />
         </View>
-      </Card>
-
-    </View>
+        <View style={styles.buttonsCardDescription}>
+          <ButtonExecutions
+            onPress={() => "nothingyet"}
+            action="cancel"
+            text="CANCELAR"
+          />
+        </View>
+      </View>
+    </Card>
   );
 };
 
@@ -85,8 +81,10 @@ let styles = StyleSheet.create({
     padding: 7,
   },
   buttonsWrap: {
-    alignSelf: "flex-end",
-    flexDirection: "column"
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    minHeight: "50%",
+    width: "100%",
   },
   cardInfo: {
     alignItems: "center",
@@ -96,22 +94,20 @@ let styles = StyleSheet.create({
     marginBottom: 5,
   },
   cardStyle: {
+    alignItems: "stretch",
     backgroundColor: colors.basic.white,
     borderRadius: 10,
-    display: "flex",
-    //height: 450,
-    justifyContent: "space-between",
-    margin: 5,
-    position: "relative",
-    width: 400,
     flex: 1,
-    borderColor: "red",
-    borderWidth: 1,
+    flexDirection: "column",
+    width: "100%",
   },
   cardTitle: {
     flexDirection: "row",
     justifyContent: "center",
     minHeight: 40,
+  },
+  container: {
+    minHeight: "30%",
   },
   imagePadding: {
     marginRight: 10,
