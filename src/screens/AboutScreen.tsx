d9cca8632e2377ababcf08ee9d29a1805619d52a
João Brentano <joaobrentano.br@gmail.com>
2020-06-15 20:12:11 -0300

    Funcionalidades base - João Brentano e Eduardo Soares

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,116 +1,95 @@
-import React, { useState } from "react";
-import { Dimensions, SafeAreaView, StyleSheet, View, Text } from "react-native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { useNavigation } from "@react-navigation/native";
+import React from "react";
 import {
-  createExecution,
-  timeToString,
-  initializeExecution,
-  cancelExecution,
-  pauseExecution,
-  finishExecution,
-  updateAllTimers,
-} from "../services/timerFunction";
-import { ButtonPrimary, InputText, ButtonExecutions } from "../components";
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+} from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import sizes from "../utils/sizes";
+import colors from "../utils/colors";
 
 export interface ScreenProps {
   navigation: StackNavigationProp<any, any>;
 }
 
-const EmptyScreen: React.FC<ScreenProps> = () => {
-  // TODO: na tela apropriada, pegar dados do paciente atual do AsyncStorage
-  const cardInfo = {
-    idPatient: "1234",
-    role: 1,
-    activity: 42,
-  };
-
-  const testTime = 100;
-  const [position, setPosition] = useState("0");
-  const navigation = useNavigation();
+const AboutScreen: React.FC<ScreenProps> = () => {
   return (
-    <SafeAreaView>
+    <ScrollView>
       <View style={styles.body}>
-        <Text>Você chegou, mas não tem nada aqui</Text>
-        <Text>
-          Use esta tela como teste, como base para você criar outra, mas NÃO
-          apague ela, mais alguém pode precisar. Quando estiver acabando o
-          projeto, removeremos
-        </Text>
-        <InputText
-          autoFocus
-          title="Posição Array"
-          onChangeText={(key) => setPosition(key)}
-          value={position}
-        />
-        <ButtonPrimary
-          title="Adiciona execução"
-          onPress={() => {
-            createExecution(cardInfo);
-          }} // remover modificação futuramente, apenas para testes
-        />
-        <ButtonPrimary
-          title="updateAllTimers"
-          onPress={() => {
-            updateAllTimers();
-          }} // remover modificação futuramente, apenas para testes
-        />
-        <ButtonPrimary
-          title="TimeToStringTest"
-          onPress={() => {
-            timeToString(testTime);
-          }} // remover modificação futuramente, apenas para testes
-        />
-        {/* TODO: botões de execução estão aqui somente para teste */}
-        <ButtonExecutions
-          onPress={() => {
-            initializeExecution(Number(position));
-          }} // remover modificação futuramente, apenas para testes
-          action="start"
-          text="INICIAR"
-        />
-        <ButtonExecutions
-          onPress={() => {
-            pauseExecution(Number(position));
-          }} // remover modificação futuramente, apenas para testes
-          action="stop"
-          text="PARAR"
-        />
-        <ButtonExecutions
-          onPress={() => {
-            finishExecution(Number(position));
-          }} // remover modificação futuramente, apenas para testes
-          action="finish"
-          text="CONCLUIR E SALVAR"
-        />
-        <ButtonExecutions
-          onPress={() => {
-            initializeExecution(Number(position));
-          }} // remover modificação futuramente, apenas para testes
-          action="restart"
-          text="RETOMAR CONTAGEM"
-        />
-        <ButtonExecutions
-          onPress={() => {
-            cancelExecution(Number(position));
-          }} // remover modificação futuramente, apenas para testes
-          action="cancel"
-          text="CANCELAR"
-        />
-        <ButtonPrimary
-          title="Inicio"
-          onPress={() => navigation.navigate("Home")}
-        />
+        <View style={styles.mainView}>
+          <Text style={styles.text}>
+            Projeto desenvolvido por alunos da Agência Experimental de
+            Engenharia de Software (AGES), da PUCRS, em parceria com os
+            idealizadores Ana Paula e Ricardo do Instituto de Avaliação de
+            Tecnologia em Saúde (IATS).
+          </Text>
+        </View>
+        <View style={styles.mainView}>
+          <Text style={styles.textTopic}>Alunos:</Text>
+          <Text style={styles.text}>
+            Alex Brustolin Teixeira, Bianca Camargo, Bruno Marcelino, Camila
+            Rocha, Eduardo André Soares, Felipe Silveira, Gabriel Gioscia
+            Velloso, Igor Sgorla Brehm, Jessica Manoel, João Brentano, Lucas
+            Castro, Marlon Furtado, Micael Fischmann, Rafael Araujo e Vinicius
+            Branco.
+          </Text>
+        </View>
+        <View style={styles.mainView}>
+          <Text style={styles.textTopic}>Stakeholders:</Text>
+          <Text style={styles.text}>
+            Ana Paula Beck da Silva Etges e Ricardo Bertoglio Cardoso.
+          </Text>
+        </View>
+        <View style={(styles.mainView, styles.inlineTitle)}>
+          <Text style={styles.textTopic}>Professor Orientador:</Text>
+          <Text style={styles.text}> Daniel Callegari</Text>
+        </View>
+        <View style={(styles.mainView, styles.inlineTitle)}>
+          <Text style={styles.textTopic}>Semestre:</Text>
+          <Text style={styles.text}> 2020/1</Text>
+        </View>
+        <View style={styles.image}>
+          <Image source={require("../assets/logo-pucrs.png")} />
+          <Image source={require("../assets/logo-ages.png")} />
+        </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
-    alignItems: "center",
     minHeight: Dimensions.get("window").height,
+    padding: 10,
+  },
+  textTopic: {
+    alignItems: "flex-start",
+
+    fontSize: sizes.inputText.textField,
+    color: colors.text.primary,
+    fontWeight: "bold",
+  },
+  text: {
+    alignItems: "flex-start",
+    fontSize: sizes.inputText.textField,
+    color: colors.text.primary,
+  },
+  image: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mainView: {
+    marginVertical: 10,
+    textAlign: "left",
+  },
+  inlineTitle: {
+    marginVertical: 10,
+    flexDirection: "row",
   },
 });
 
-export default EmptyScreen;
+export default AboutScreen;
