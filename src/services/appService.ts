145989f1104bcccbb62f55c61d2b1ea588c2cbec
Rafael Victor Ruwer Araujo <rafaelruweraraujo@gmail.com>
2020-06-29 12:45:35 -0300

    Enviar endDate para backend

--- src/services/appService.ts
+++ src/services/appService.ts
@@ -172,6 +172,7 @@ const uploadExecutions = async (): Promise<void> => {
       activityId: execution.activity,
       roleId: execution.role,
       timestamp: moment(execution.date).toISOString(true),
+      endDate: moment(execution.endDate).toISOString(true),
       duration: execution.duration,
     };
   });
